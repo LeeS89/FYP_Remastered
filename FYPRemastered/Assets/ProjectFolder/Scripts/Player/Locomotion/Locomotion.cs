4bd9297625d850ce00086856309fb5a142c52098
LeeS89 <leeslammon@outlook.com>
2025-02-26 03:25:52 +0000

    Setup Gravity and completed locomotion

--- FYPRemastered/Assets/ProjectFolder/Scripts/Player/Locomotion/Locomotion.cs
+++ FYPRemastered/Assets/ProjectFolder/Scripts/Player/Locomotion/Locomotion.cs
@@ -1,5 +1,4 @@
 using Oculus.Interaction;
-using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class Locomotion : MonoBehaviour, IBindableToPlayerEvents
@@ -11,11 +10,14 @@ public class Locomotion : MonoBehaviour, IBindableToPlayerEvents
     [SerializeField] private float _bodyHeightMax = 2f;
     private Vector3 _newControllerCenter = Vector3.zero;
     [SerializeField] private float _moveSpeed = 4.0f;
-    [SerializeField]
-    private bool _shouldMoveForward = false;
+    [SerializeField] private bool _shouldMoveForward = false;
     private const float GRAVITY = -9.8f;
     private float velocityY = 0f;
     private Vector3 velocity = Vector3.zero;
+    private Vector3 _moveDirection = Vector3.zero;
+    private float _effectiveMoveSpeed = 0f;
+
+    float newVelocityY = 0f;
     //End
 
 
@@ -46,7 +48,7 @@ public class Locomotion : MonoBehaviour, IBindableToPlayerEvents
 
     private void Update()
     {
-        MoveForward();
+        ApplyPlayerMovement();
     }
 
     private void HandleRotation(Quaternion targetRotation)
@@ -56,57 +58,54 @@ public class Locomotion : MonoBehaviour, IBindableToPlayerEvents
 
     private void AdjustPlayerHeight(Vector3 _cameraLocalPos)
     {
-        if (_controller != null) { return; }
+        if (_controller == null) { return; }
 
         _controller.height = Mathf.Clamp(_cameraLocalPos.y, _bodyHeightMin, _bodyHeightMax);
         _newControllerCenter.x = _cameraLocalPos.x;
         _newControllerCenter.y = _controller.height / 2;
         _newControllerCenter.z = _cameraLocalPos.z;
 
+
+
         _controller.center = _newControllerCenter;
 
     }
 
-    private void MoveForward()
-    {
-        if (_controller == null) { return; }
-
-        if (!_shouldMoveForward && !CheckIfGravityShouldBeApplied(ref velocityY)) { return; }
-
-        float effectiveMoveSpeed = _shouldMoveForward ? _moveSpeed : 0f;
-
-        Vector3 moveDirection = transform.forward;
-        velocity = moveDirection * effectiveMoveSpeed;
-        velocity.y = velocityY;
-
-        _controller.Move(velocity * Time.deltaTime);
-    }
-
+   
     private void SetShouldMoveforward(bool move)
     {
         _shouldMoveForward = move;
     }
 
-    private bool CheckIfGravityShouldBeApplied(ref float newVelocityY)
-    {
-        if (_controller.isGrounded)
-        {
-            newVelocityY = 0f;
-            return false;
-        }
 
-        newVelocityY += GRAVITY * Time.deltaTime;
-        return true;
-    }
 
-    /*private void MoveForward()
+    private void ApplyPlayerMovement()
     {
         if (_controller == null) { return; }
 
+        _effectiveMoveSpeed = _shouldMoveForward ? _moveSpeed : 0f;
 
+        _moveDirection = transform.forward;
+        velocity = _moveDirection * _effectiveMoveSpeed;
+        velocity.y = ApplyGravity();
 
-        Vector3 moveDirection = _controller.transform.forward;
+        _controller.Move(velocity * Time.deltaTime);
 
-        _controller.Move(moveDirection * _moveSpeed * Time.deltaTime);
-    }*/
+    }
+
+    private float ApplyGravity()
+    {
+        if(_controller.isGrounded)
+        {
+            newVelocityY = GRAVITY / 2f;
+        }
+        else
+        {
+            newVelocityY += GRAVITY * Time.deltaTime;
+        }
+       
+        return newVelocityY;
+    }
+
+    
 }
